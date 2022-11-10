3b2bd0dd2b0ed66f89d7451ac655e47dac2d342e
Kyle Hodgson <kyle.hodgson@gmail.com>
2022-11-10 14:31:34 -0500

    refactoring, fixing a bug with detecting environment variables

--- src/simplemoer/client.py
+++ src/simplemoer/client.py
@@ -6,48 +6,58 @@ from requests.auth import HTTPBasicAuth
 import json
 import os
 
+
 class WattTime:
-    token=""
-    ba=""
+    token = ""
+    ba = ""
     INDEX_URL = 'https://api2.watttime.org/index'
     LOGIN_URL = 'https://api2.watttime.org/v2/login'
     REGION_URL = 'https://api2.watttime.org/v2/ba-from-loc'
+
     def __init__(self, username="", password="", latt="", long="") -> None:
-        if username=="":
-            username=os.environ.get('WATTTIMEUSERNAME')
-        if password=="": 
-            password=os.environ.get('WATTTIMEPASSWORD')
-        if latt=="":
-            latt=os.environ.get('LATT')
-        if long=="":
-            long=os.environ.get('LONG')
+        if not username:
+            username = os.environ.get('WATTTIMEUSERNAME')
+        if not password:
+            password = os.environ.get('WATTTIMEPASSWORD')
+        if not latt:
+            latt = os.environ.get('LATT')
+        if not long:
+            long = os.environ.get('LONG')
 
-        if username=="" or password=="" or latt=="" or long=="":
-            raise Exception("Did not find watttime credentials. Either provide them while enstantiating WattTime, or make sure the WATTTIMEUSERNAME, WATTTIMEPASSWORD, LATT and LONG environment variables are set.")
+        if not username or not password or not latt or not long:
+            raise Exception(
+                "Did not find watttime credentials or GPS coordinates. "
+                "Either provide them while enstantiating WattTime, or "
+                "make sure the WATTTIMEUSERNAME, WATTTIMEPASSWORD, LATT "
+                "and LONG environment variables are set.")
 
-        resp_plain = requests.get(self.LOGIN_URL, auth=HTTPBasicAuth(username, password))
-        self.token=resp_plain.json()['token']
-        
-        ba_resp=self.get_BA(latt,long)
-        self.ba=ba_resp['abbrev']
+        resp_plain = requests.get(
+            self.LOGIN_URL, auth=HTTPBasicAuth(username, password))
+        self.token = resp_plain.json()['token']
 
+        ba_resp = self.get_BA(latt, long)
+        self.ba = ba_resp['abbrev']
+        return
 
     def get_index(self, ba=""):
-        if self.token=="":
+        if self.token == "":
             raise InvalidOperation("please login first.")
-        if ba=="":
-            ba=self.ba
+        if ba == "":
+            ba = self.ba
 
         headers = {'Authorization': 'Bearer {}'.format(self.token)}
         params = {'ba': '{}'.format(ba)}
-        resp_text=requests.get(self.INDEX_URL, headers=headers, params=params)
-        index=resp_text.json()
+        resp_text = requests.get(
+            self.INDEX_URL, headers=headers, params=params)
+        index = resp_text.json()
         return index
 
-    def get_BA(self, latt, long)->string:
-        if self.token=="":
+    def get_BA(self, latt, long) -> string:
+        if self.token == "":
             raise InvalidOperation("please login first.")
         headers = {'Authorization': 'Bearer {}'.format(self.token)}
-        params = {'latitude': '{}'.format(latt), 'longitude': '{}'.format(long)}
-        resp_plain=requests.get(self.REGION_URL, headers=headers, params=params)
+        params = {'latitude': '{}'.format(
+            latt), 'longitude': '{}'.format(long)}
+        resp_plain = requests.get(
+            self.REGION_URL, headers=headers, params=params)
         return resp_plain.json()
